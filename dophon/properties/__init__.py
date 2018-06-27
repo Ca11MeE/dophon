e42fe442cdb740ad9b964a77ed924ecc4b6b1bf3
CallMeE <ealohu@163.com>
2018-06-27 22:45:00 +0800

    进化配置文件为配置管理与默认配置结合

--- dophon/properties/__init__.py
+++ dophon/properties/__init__.py
@@ -1,16 +1,19 @@
 # coding: utf-8"
+import sys
 """
 配置集合
 author:CallMeE
 date:2018-06-01
 """
+
+print('引用默认配置')
 # 此处为服务器配置
 host = '127.0.0.1'
 port = 443
 ssl_context = 'adhoc'
 
 # 此处为蓝图文件夹配置
-blueprint_path = ['routes_package_path']
+blueprint_path = ['/routes_package_path']
 pool_conn_num = 'db_connect_pool_size'
 
 # 此处为数据库配置
@@ -19,3 +22,8 @@ pydc_port = 3306
 pydc_user = 'username'
 pydc_password = 'password'
 pydc_database = 'database'
+
+
+def get_properties(prop_path=None):
+    if prop_path:
+        sys.modules['properties']=__import__(prop_path,fromlist=True)
