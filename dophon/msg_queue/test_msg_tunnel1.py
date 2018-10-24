3d4389625d86a2874cf9fb969e834fd657d6cf54
CallMeE <ealohu@163.com>
2018-10-25 06:30:13 +0800

    分离消息队列

--- dophon/msg_queue/test_msg_tunnel1.py
+++
@@ -1,8 +0,0 @@
-from dophon.msg_queue.MsgCenter import SocketMsgTunnel
-
-if __name__ == '__main__':
-    SocketMsgTunnel('DEMO_TAGi').send_msg('哈哈哈')
-    SocketMsgTunnel('DEMO_TAGa').send_msg('哈哈哈')
-    SocketMsgTunnel('DEMO_TAGb').send_msg('哈哈哈')
-    while True:
-        pass
--- dophon/msg_queue/test_remote_center.py
+++
@@ -1,3 +0,0 @@
-from dophon.msg_queue.remote import remote_center
-
-remote_center.MsgCenter()
