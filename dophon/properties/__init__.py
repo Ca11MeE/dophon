6ae098179789b05c017035d93bd0c873bdd35baf
CallMeE <ealohu@163.com>
2018-11-25 03:11:18 +0800

    优化配置模块,启动更智能(配合dophon-manager),自动识别配置文件并添加环境,新增测试包(作为demo可启动)

--- dophon/properties/__init__.py
+++ dophon/properties/__init__.py
@@ -7,14 +7,16 @@
 """
 import sys
 import logging
-import os, re, json
-from datetime import datetime
-from threading import *
+import re
+import os
+
+re_import_prop_flag = False
 
 
 def read_self_prop():
+    global re_import_prop_flag
     try:
-        def_prop = __import__('dophon.def_prop.default_properties', fromlist=True)
+        def_prop = __import__('dophon.def_prop.__init__', fromlist=True)
         u_prop = __import__('application', fromlist=True)
         # 对比配置文件
         for name in dir(def_prop):
@@ -26,10 +28,21 @@ def read_self_prop():
         sys.modules['properties'] = u_prop
         sys.modules['dophon.properties'] = u_prop
     except Exception as e:
-        logging.error(e)
-        sys.modules['properties'] = def_prop
-        sys.modules['dophon.properties'] = def_prop
-        raise e
+        # 添加项目路径到环境变量
+        logging.info('添加项目路径到环境变量: %s' % (os.getcwd(),))
+        for (root, dirs, files) in os.walk(os.getcwd(), topdown=True):
+            if 'application.py' in files:
+                sys.path.append(root)
+                break
+        if not re_import_prop_flag:
+            logging.info('重新引入配置')
+            re_import_prop_flag = True
+            read_self_prop()
+        else:
+            logging.warning('重新引入配置失败')
+            sys.modules['properties'] = def_prop
+            sys.modules['dophon.properties'] = def_prop
+            raise e
 
 
 try:
@@ -37,3 +50,18 @@ try:
 except Exception as e:
     logging.error('没有找到自定义配置:(application.py)')
     logging.error('引用默认配置')
+    logging.info('创建配置文件: application.py')
+    def_prop = __import__('dophon.def_prop.default_properties', fromlist=True)
+    with open(os.getcwd() + os.sep + 'application.py', 'wb') as app_prop:
+        for name in dir(def_prop):
+            if re.match('__.*__', name):
+                continue
+            else:
+                value = getattr(def_prop, name)
+                if type(value) is type(os):
+                    continue
+                app_prop.write(
+                    bytes(name + '=' + re.sub('\\\\', '/',
+                                              str(('\'' + value + '\'') if isinstance(value, str) else value)) + '\n',
+                          encoding='utf-8'))
+    read_self_prop()
